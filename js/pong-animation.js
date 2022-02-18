d62bd185810fb240065e75fda08361245f3f2566
Mark Bucholski <mbucholski95@gmail.com>
2022-02-18 00:54:30 -0500

    seventeenth commit

--- js/pong-animation.js
+++ js/pong-animation.js
@@ -14,7 +14,10 @@ let interval;
 // Variables for the difference in x and y values from initial hit to first bounce and lastPosition after each animation
 let differenceX;
 let differenceY;
-let lastPosition;
+let lastBallPosition;
+let lastRacket1Postion;
+let lastRacket2Position;
+let animationNumber = 0;
 
 // Used for rounding the third decimal place
 const round = (num) => {
@@ -28,8 +31,8 @@ const farHit = (animationNumber) => {
     if (animationNumber !== 1) {
         // Move racket 1 and ball to same position based off of direction ball was heading in after leaving racket 2.
     }
-    racket1.setAttribute("animation__rotationForward", "property: rotation; to: 80 80 0; dur: 500; easing: easeInQuad;");
-    racket1.setAttribute("animation__rotationBackward", `property: rotation; to: ${originalRotationX} 80 0; dur: 500; delay: 1000; easing: easeOutQuad;`);
+    racket1.setAttribute("animation__racket1__rotationForward", "property: rotation; to: 80 80 0; dur: 500; easing: easeInQuad;");
+    racket1.setAttribute("animation__racket1__rotationBackward", `property: rotation; to: ${originalRotationX} 80 0; dur: 500; delay: 1000; easing: easeOutQuad;`);
 }
 
 // Determines the exact position of the far bounce and animates the ball to move to the specified position
@@ -39,29 +42,42 @@ const farBounce = () => {
     const ballPosition = ball.getAttribute("position");
     differenceX = round((ballPosition.x - xBounce) * -1);
     differenceY = round((ballPosition.y - yBounce) * -1);
-    lastPosition = { x: xBounce, y: yBounce, z: borders.zBottom }; 
+    lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom }; 
     ball.setAttribute("animation__nearBounce", `property: position; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 250; easing: linear;`);
 }
 
 // Animates the ball to move directly above net
 const netHop = () => {
-    const xHop = (lastPosition.x + differenceX) / 2;
+    const xHop = (lastBallPosition.x + differenceX) / 2;
     const zHop = round((Math.random() * -0.075) + -0.185);
-    ball.setAttribute("animation__hop", `property: position; from: ${lastPosition.x} ${lastPosition.y} ${lastPosition.z}; to: ${xHop} 0 ${zHop}; dur: 250; delay: 250; ease: easeInCirc;`);
-    lastPosition = { x: xHop, y: 0, z: zHop };
+    ball.setAttribute("animation__hop", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xHop} 0 ${zHop}; dur: 250; delay: 250; ease: easeInCirc;`);
+    lastBallPosition = { x: xHop, y: 0, z: zHop };
 }
 
 // Animates ball to bounce on near side of the ping-pong table
 const nearBounce = () => {
-    const xBounce = (lastPosition.x + differenceX);
-    const yBounce = (lastPosition.y + differenceY + (Math.random() * 0.3));
-    ball.setAttribute("animation__farBounce", `property: position; from: ${lastPosition.x} ${lastPosition.y} ${lastPosition.z}; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 500; delay: 500; ease: easeOutCirc;`);
-    lastPosition = { x: xBounce, y: yBounce, z: borders.zBottom };
+    const xBounce = lastBallPosition.x + differenceX / 1.25;
+    const yBounce = (lastBallPosition.y + differenceY + (Math.random() * 0.3));
+    ball.setAttribute("animation__farBounce", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 500; delay: 500; ease: easeOutCirc;`);
+    lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom };
 }
 
 // Animates racket to hit ball on near side of the ping-pong table
 const nearHit = () => {
-
+    if ( animationNumber === 1 ) {
+        const position = racket2.getAttribute("position");
+        lastRacket2Position = { x: position.x, y: position.y, z: position.z };
+    }
+    const originalRotationX = racket2.getAttribute("rotation").x;
+    const hitPositionX = lastBallPosition.x + differenceX;
+    const hitPositionY = (lastBallPosition.y + differenceY + (Math.random() * 0.3));
+    const hitPositionZ = round((Math.random() * -0.1) + -0.185);
+    ball.setAttribute("animation__nearHit", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${hitPositionX - 0.1} ${hitPositionY + 0.015} ${hitPositionZ}; dur: 500; delay: 1000; ease: easeOutQuad;`);
+    racket2.setAttribute("animation__position", `property: position; from: ${lastRacket2Position.x} ${lastRacket2Position.y} ${lastRacket2Position.z}; to: ${hitPositionX} ${hitPositionY} ${hitPositionZ}; dur: 500; delay: 1000; ease: linear;`);
+    racket2.setAttribute("animation__racket2__rotationForward", "property: rotation; to: 190 260 0; dur: 250; delay: 1500; easing: easeInQuad;");
+    racket2.setAttribute("animation__rotationBackward", `property: rotation; to: ${originalRotationX} 260 0; dur: 500; delay: 2500; easing: easeOutQuad;`);
+    lastRacket2Position = { x: hitPositionX, y: hitPositionY, z: hitPositionZ };
+    lastBallPosition = { x: hitPositionX, y: hitPositionY, z: hitPositionZ };
 }
 
 // After page load the scene is defined and a series of animations will be executed when the marker is found
@@ -84,18 +100,20 @@ window.onload = () => {
                 side.setAttribute("animation", `property: position; from: ${index + 1} ${index + 1} ${index + 1}; to: ${position.x} ${position.y} ${position.z}; dur: 7000; easing: easeOutQuad;`); 
             } )
 
-            let animationNumber = 0;
-
 // The various functions which contain the ping pong playing animation are executed below after opening animation
 // setTimeout is used to start ping pong animation after opening animation
 // setInterval allows for the main ping pong animation to continue infinetly
             timeout = setTimeout(() => {
                     animationNumber += 1;
-                    farHit(animationNumber);
+                    farHit();
                     farBounce();
                     netHop();
                     nearBounce();
                     nearHit();
+                    // nearBounce();
+                    // netHop();
+                    // farBounce();
+                    // farHit();
             }, 7000)
         }
     } )

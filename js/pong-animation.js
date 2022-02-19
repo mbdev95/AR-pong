029ad5e745c83d082b7bb2b1246b2b4013b3dfc6
Mark Bucholski <mbucholski95@gmail.com>
2022-02-19 00:49:16 -0500

    eighteenth commit

--- js/pong-animation.js
+++ js/pong-animation.js
@@ -18,6 +18,7 @@ let lastBallPosition;
 let lastRacket1Postion;
 let lastRacket2Position;
 let animationNumber = 0;
+let direction;
 
 // Used for rounding the third decimal place
 const round = (num) => {
@@ -30,36 +31,64 @@ const farHit = (animationNumber) => {
     const originalRotationX = racket1.getAttribute("rotation").x;
     if (animationNumber !== 1) {
         // Move racket 1 and ball to same position based off of direction ball was heading in after leaving racket 2.
+        // const randomNegPos = Math.random() < 0.5 ? -1 : 1;
     }
-    racket1.setAttribute("animation__racket1__rotationForward", "property: rotation; to: 80 80 0; dur: 500; easing: easeInQuad;");
+    racket1.setAttribute("animation__racket1__rotationForward", "property: rotation; to: 80 80 0; dur: 250; easing: easeInQuad;");
     racket1.setAttribute("animation__racket1__rotationBackward", `property: rotation; to: ${originalRotationX} 80 0; dur: 500; delay: 1000; easing: easeOutQuad;`);
+    direction="forwards";
 }
 
 // Determines the exact position of the far bounce and animates the ball to move to the specified position
 const farBounce = () => {
-    const xBounce = round((Math.random() *  (borders.xLeft / 2)));
-    const yBounce = round((Math.random() * (borders.yBack / 2)) - 0.2 );
-    const ballPosition = ball.getAttribute("position");
-    differenceX = round((ballPosition.x - xBounce) * -1);
-    differenceY = round((ballPosition.y - yBounce) * -1);
-    lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom }; 
-    ball.setAttribute("animation__nearBounce", `property: position; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 250; easing: linear;`);
+    if ( direction === "forwards" ) {
+        const xBounce = round((Math.random() *  (borders.xLeft / 2)));
+        const yBounce = round((Math.random() * (borders.yBack / 2)) - 0.2 );
+        const ballPosition = ball.getAttribute("position");
+        differenceX = round((ballPosition.x - xBounce) * -1);
+        differenceY = round((ballPosition.y - yBounce) * -1);
+        ball.setAttribute("animation__nearBounce__forwards", `property: position; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 350; easing: linear;`);
+        lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom }; 
+    } else {
+        const xBounce = round((lastBallPosition.x + differenceX) / 1.75);
+        console.log(lastBallPosition.x + " " + differenceX + " " + xBounce);
+        const yBounce = round(((lastBallPosition.y + differenceY) * -1) / 1.4);
+        ball.setAttribute("animation__nearBounce__backwards", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 350; delay: 2100; easing: easeOutCirc;`);
+        lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom }; 
+    }
 }
 
 // Animates the ball to move directly above net
 const netHop = () => {
-    const xHop = (lastBallPosition.x + differenceX) / 2;
+    const xHop = round(((lastBallPosition.x + differenceX) / 2));
     const zHop = round((Math.random() * -0.075) + -0.185);
-    ball.setAttribute("animation__hop", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xHop} 0 ${zHop}; dur: 250; delay: 250; ease: easeInCirc;`);
+    if ( direction === "forwards" ) {
+        ball.setAttribute("animation__hopForward", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xHop} 0 ${zHop}; dur: 350; delay: 350; easing: easeInCirc;`);
+    } else {
+        // console.log(lastBallPosition.x + " " + differenceX + " " + xHop);
+        ball.setAttribute("animation__hopBackward", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xHop} 0 ${zHop}; dur: 350; delay: 1750; easing: easeInCirc;`);
+    }
     lastBallPosition = { x: xHop, y: 0, z: zHop };
 }
 
 // Animates ball to bounce on near side of the ping-pong table
 const nearBounce = () => {
-    const xBounce = lastBallPosition.x + differenceX / 1.25;
-    const yBounce = (lastBallPosition.y + differenceY + (Math.random() * 0.3));
-    ball.setAttribute("animation__farBounce", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 500; delay: 500; ease: easeOutCirc;`);
-    lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom };
+    if ( direction === "forwards" ) {
+        const xBounce = round(lastBallPosition.x + differenceX / 1.25);
+        const yBounce = round(lastBallPosition.y + differenceY + (Math.random() * 0.3));
+        ball.setAttribute("animation__nearBackwardBounce", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 350; delay: 700; easing: easeOutCirc;`);
+        lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom };
+    } else {
+        const randomNegPos = Math.random() < 0.5 ? -1 : 1;
+        const xBounce = round(((Math.random() *  (borders.xRight / 2)) * randomNegPos));
+        const yBounce = round((Math.random() * (borders.yFront) / 2.5) + 0.2 );
+        differenceX = round((lastBallPosition.x - xBounce) / 1.65);
+        if ( lastBallPosition.x > xBounce ) {
+            differenceX = differenceX * -1;
+        }
+        differenceY = round(lastBallPosition.y - yBounce);
+        ball.setAttribute("animation__nearForwardBounce", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${xBounce} ${yBounce} ${borders.zBottom}; dur: 350; delay: 1400; easing: linear;`);
+        lastBallPosition = { x: xBounce, y: yBounce, z: borders.zBottom }; 
+    }
 }
 
 // Animates racket to hit ball on near side of the ping-pong table
@@ -69,15 +98,16 @@ const nearHit = () => {
         lastRacket2Position = { x: position.x, y: position.y, z: position.z };
     }
     const originalRotationX = racket2.getAttribute("rotation").x;
-    const hitPositionX = lastBallPosition.x + differenceX;
-    const hitPositionY = (lastBallPosition.y + differenceY + (Math.random() * 0.3));
+    const hitPositionX = round(lastBallPosition.x + differenceX);
+    const hitPositionY = round(lastBallPosition.y + differenceY + (Math.random() * 0.3));
     const hitPositionZ = round((Math.random() * -0.1) + -0.185);
-    ball.setAttribute("animation__nearHit", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${hitPositionX - 0.1} ${hitPositionY + 0.015} ${hitPositionZ}; dur: 500; delay: 1000; ease: easeOutQuad;`);
-    racket2.setAttribute("animation__position", `property: position; from: ${lastRacket2Position.x} ${lastRacket2Position.y} ${lastRacket2Position.z}; to: ${hitPositionX} ${hitPositionY} ${hitPositionZ}; dur: 500; delay: 1000; ease: linear;`);
-    racket2.setAttribute("animation__racket2__rotationForward", "property: rotation; to: 190 260 0; dur: 250; delay: 1500; easing: easeInQuad;");
-    racket2.setAttribute("animation__rotationBackward", `property: rotation; to: ${originalRotationX} 260 0; dur: 500; delay: 2500; easing: easeOutQuad;`);
+    ball.setAttribute("animation__nearHit__ballPosition", `property: position; from: ${lastBallPosition.x} ${lastBallPosition.y} ${lastBallPosition.z}; to: ${hitPositionX - 0.1} ${hitPositionY + 0.015} ${hitPositionZ}; dur: 350; delay: 1050; easing: linear;`);
+    racket2.setAttribute("animation__nearHit__racket2Position", `property: position; from: ${lastRacket2Position.x} ${lastRacket2Position.y} ${lastRacket2Position.z}; to: ${hitPositionX} ${hitPositionY} ${hitPositionZ}; dur: 350; delay: 1050; easing: easeOutQuad;`);
+    racket2.setAttribute("animation__racket2__rotateForward", "property: rotation; to: 190 260 0; dur: 175; delay: 1400; easing: easeInQuad;");
+    racket2.setAttribute("animation__racket2__rotateBackward", `property: rotation; to: ${originalRotationX} 260 0; dur: 500; delay: 2500; easing: easeOutQuad;`);
     lastRacket2Position = { x: hitPositionX, y: hitPositionY, z: hitPositionZ };
     lastBallPosition = { x: hitPositionX, y: hitPositionY, z: hitPositionZ };
+    direction="backwards";
 }
 
 // After page load the scene is defined and a series of animations will be executed when the marker is found
@@ -110,9 +140,9 @@ window.onload = () => {
                     netHop();
                     nearBounce();
                     nearHit();
-                    // nearBounce();
-                    // netHop();
-                    // farBounce();
+                    nearBounce();
+                    netHop();
+                    farBounce();
                     // farHit();
             }, 7000)
         }
